d5cbd8b0e611fdc9a8b55fd1d881c6b55cf8e476
Maxime Lamothe-Brassard <maxime@refrctionpoint.com>
2022-11-12 16:36:08 -0800

    Tweak test filter.

--- snapattack_test.go
+++ snapattack_test.go
@@ -11,7 +11,80 @@ func TestSignatureExport(t *testing.T) {
 	c := NewClient(os.Getenv("SNAPATTACK_API_KEY"))
 
 	filter := Filter{}
-	err := json.Unmarshal([]byte(`{"op":"and","items":[{"op":"in","field":"organization_id","value":[2147483627]},{"op":"or","items":[{"op":"equals","field":"gap","value":"true"},{"op":"equals","field":"validated","value":"true"}]},{"op":"in","field":"visibility","value":["Published"]},{"op":"for_each","items":[{"op":"in","field":"ranks.rank","value":["Highest","High","Medium"]},{"op":"equals","field":"ranks.organization_id","value":104,"case_sensitive":true}]},{"op":"any","field":"analytic_compilation_targets","value":[83]}]}`), &filter)
+	err := json.Unmarshal([]byte(`{
+		"op": "and",
+		"items": [
+		  {
+			"op": "or",
+			"items": [
+			  {
+				"op": "equals",
+				"field": "gap",
+				"value": "true"
+			  },
+			  {
+				"op": "equals",
+				"field": "validated",
+				"value": "true"
+			  }
+			]
+		  },
+		  {
+			"op": "in",
+			"field": "visibility",
+			"value": [
+			  "Published"
+			]
+		  },
+		  {
+			"op": "for_each",
+			"items": [
+			  {
+				"op": "in",
+				"field": "ranks.rank",
+				"value": [
+				  "Highest",
+				  "High",
+				  "Medium"
+				]
+			  },
+			  {
+				"op": "equals",
+				"field": "ranks.organization_id",
+				"value": 168,
+				"case_sensitive": true
+			  }
+			]
+		  },
+		  {
+			"op": "for_each",
+			"items": [
+			  {
+				"op": "in",
+				"field": "severities.severity_name",
+				"value": [
+				  "Highest",
+				  "High",
+				  "Medium"
+				]
+			  },
+			  {
+				"op": "equals",
+				"field": "severities.organization_id",
+				"value": 168,
+				"case_sensitive": true
+			  }
+			]
+		  },
+		  {
+			"op": "any",
+			"field": "analytic_compilation_targets",
+			"value": [
+			  83
+			]
+		  }
+		]
+	  }`), &filter)
 	if err != nil {
 		t.Errorf("json.Unmarshal() filter: %v", err)
 		return
